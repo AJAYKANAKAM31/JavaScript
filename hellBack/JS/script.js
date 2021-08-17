d4d840c27c0e01b49a5680608d6e9e103857ea85
AJAYKANAKAM31 <ajaykanakam31@gmail.com>
2021-08-17 09:10:01 +0530

    Add files via upload

---
+++ hellBack/JS/script.js
@@ -0,0 +1,34 @@
+setTimeout(() => {
+  document.getElementById("change").innerHTML = "10";
+  setTimeout(() => {
+    document.getElementById("change").innerHTML = "9";
+    setTimeout(() => {
+      document.getElementById("change").innerHTML = "8";
+      setTimeout(() => {
+        document.getElementById("change").innerHTML = "7";
+        setTimeout(() => {
+          document.getElementById("change").innerHTML = "6";
+          setTimeout(() => {
+            document.getElementById("change").innerHTML = "5";
+            setTimeout(() => {
+              document.getElementById("change").innerHTML = "4";
+              setTimeout(() => {
+                document.getElementById("change").innerHTML = "3";
+                setTimeout(() => {
+                  document.getElementById("change").innerHTML = "2";
+                  setTimeout(() => {
+                    document.getElementById("change").innerHTML = "1";
+                    setTimeout(() => {
+                      document.getElementById("change").innerHTML =
+                        "Happy NEW YEAR!!!";
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
