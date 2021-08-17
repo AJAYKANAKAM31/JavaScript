41dca32d5d332eceacfd9e0f3185865d3cac288e
AJAYKANAKAM31 <ajaykanakam31@gmail.com>
2021-08-17 09:12:36 +0530

    Update script.js

--- hellBack/JS/script.js
+++ hellBack/JS/script.js
@@ -1,27 +1,24 @@
 setTimeout(() => {
-  document.getElementById("change").innerHTML = "10";
+  document.getElementById("change").innerHTML = "9";
   setTimeout(() => {
-    document.getElementById("change").innerHTML = "9";
+    document.getElementById("change").innerHTML = "8";
     setTimeout(() => {
-      document.getElementById("change").innerHTML = "8";
+      document.getElementById("change").innerHTML = "7";
       setTimeout(() => {
-        document.getElementById("change").innerHTML = "7";
+        document.getElementById("change").innerHTML = "6";
         setTimeout(() => {
-          document.getElementById("change").innerHTML = "6";
+          document.getElementById("change").innerHTML = "5";
           setTimeout(() => {
-            document.getElementById("change").innerHTML = "5";
+            document.getElementById("change").innerHTML = "4";
             setTimeout(() => {
-              document.getElementById("change").innerHTML = "4";
+              document.getElementById("change").innerHTML = "3";
               setTimeout(() => {
-                document.getElementById("change").innerHTML = "3";
+                document.getElementById("change").innerHTML = "2";
                 setTimeout(() => {
-                  document.getElementById("change").innerHTML = "2";
+                  document.getElementById("change").innerHTML = "1";
                   setTimeout(() => {
-                    document.getElementById("change").innerHTML = "1";
-                    setTimeout(() => {
-                      document.getElementById("change").innerHTML =
-                        "Happy NEW YEAR!!!";
-                    }, 1000);
+                    document.getElementById("change").innerHTML =
+                      "Happy NEW YEAR!!!";
                   }, 1000);
                 }, 1000);
               }, 1000);
