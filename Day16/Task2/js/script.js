1ee7ee9fd4b11b249b7fbd77fded9892bc3d93fa
AJAYKANAKAM31 <ajaykanakam31@gmail.com>
2021-08-19 12:30:07 +0530

    Add files via upload

---
+++ Day16/Task2/js/script.js
@@ -0,0 +1,138 @@
+var obj = [
+  {
+    eventno: "Event 1",
+    image: "IMG/img1.jpg",
+    eventdate: "23-May-2020",
+    seats: "Seats available: 23",
+    available: "Book Now",
+  },
+  {
+    eventno: "Event 2",
+    image: "IMG/img2.jpg",
+    eventdate: "23-May-2020",
+    seats: "Seats available: 23",
+    available: "Book Now",
+  },
+  {
+    eventno: "Event 3",
+    image: "IMG/img3.jpg",
+    eventdate: "23-May-2020",
+    seats: "Seats available: 0",
+    available: "Sold out",
+  },
+  {
+    eventno: "Event 4",
+    image: "IMG/img4.jpg",
+    eventdate: "23-May-2020",
+    seats: "Seats available: 23",
+    available: "Book Now",
+  },
+  {
+    eventno: "Event 5",
+    image: "IMG/img5.jpg",
+    eventdate: "23-May-2020",
+    seats: "Seats available: 23",
+    available: "Book Now",
+  },
+  {
+    eventno: "Event 6",
+    image: "IMG/img6.jpg",
+    eventdate: "23-May-2020",
+    seats: "Seats available: 0",
+    available: "Sold out",
+  },
+  {
+    eventno: "Event 7",
+    image: "IMG/img7.jpg",
+    eventdate: "23-May-2020",
+    seats: "Seats available: 23",
+    available: "Book Now",
+  },
+  {
+    eventno: "Event 8",
+    image: "IMG/img8.jpg",
+    eventdate: "23-May-2020",
+    seats: "Seats available: 23",
+    available: "Book Now",
+  },
+  {
+    eventno: "Event 9",
+    image: "IMG/img9.jfif",
+    eventdate: "23-May-2020",
+    seats: "Seats available: 23",
+    available: "Book Now",
+  },
+
+  {
+    eventno: "Event 10",
+    image: "IMG/img10.jpg",
+    eventdate: "23-May-2020",
+    seats: "Seats available: 23",
+    available: "Book Now",
+  },
+
+  {
+    eventno: "Event 11",
+    image: "IMG/img11.jpg",
+    eventdate: "23-May-2020",
+    seats: "Seats available: 0",
+    available: "Sold Out",
+  },
+
+  {
+    eventno: "Event 12",
+    image: "IMG/img12.jpg",
+    eventdate: "23-May-2020",
+    seats: "Seats available: 23",
+    available: "Book Now",
+  },
+];
+// console.log(obj[0].eventno);
+
+function popup() {
+  window.alert("No Tickets available");
+}
+
+function opennew() {
+  window.open("#", "_blank");
+}
+
+var block = document.createElement("div");
+block.id = "main-container";
+document.body.append(block);
+
+for (let i = 0; i < obj.length; i++) {
+  var container = document.createElement("div");
+  container.className = "container";
+  block.append(container);
+
+  var eventno = document.createElement("h3");
+  eventno.innerHTML = `${obj[i].eventno}`;
+  container.append(eventno);
+
+  var imgblock = document.createElement("div");
+  imgblock.className = "block";
+  container.append(imgblock);
+
+  var main = document.createElement("div");
+  main.className = "main";
+  main.innerHTML = `
+  <img src=${obj[i].image} alt="" />`;
+  imgblock.append(main);
+
+  var content = document.createElement("div");
+  content.className = "content";
+  if (i == 2 || i == 5 || i == 10) {
+    content.innerHTML = `
+  <p>${obj[i].eventdate}</p>
+  <p>${obj[i].seats}</p>
+  <button class="soldout-option" onclick = "popup()">${obj[i].available}</button>`;
+    imgblock.append(content);
+  } else {
+    content.innerHTML = `
+    <p>${obj[i].eventdate}</p>
+    <p>${obj[i].seats}</p>
+    <button onclick="opennew()">${obj[i].available}</button>`;
+    imgblock.append(content);
+  }
+}
